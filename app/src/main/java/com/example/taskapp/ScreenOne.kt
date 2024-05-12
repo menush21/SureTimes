45fe4b2d0b1f65a460bc6a084a15d13b020a0269
Menusha malith <166796306+menush21@users.noreply.github.com>
2024-05-12 21:31:00 +0530

    SureTime

---
+++ app/src/main/java/com/example/taskapp/ScreenOne.kt
@@ -0,0 +1,23 @@
+package com.example.taskapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class ScreenOne : AppCompatActivity() {
+    private lateinit var logoLoad: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_one)
+        // Initialize medlogoLoad
+        logoLoad = findViewById(R.id.logoLod)
+        logoLoad.alpha = 0f
+        logoLoad.animate().setDuration(2000).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java) // Replace NextActivity with the actual name of your next activity
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
